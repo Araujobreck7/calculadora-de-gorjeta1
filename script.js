763a47defac95edcbe11884c0db079955900737c
Araujobreck <og.araujo7@gmail.com>
2026-05-23 14:18:56 -0300

    quarto commit do JS

--- script.js
+++ script.js
@@ -29,10 +29,10 @@ function receberQuantidadedepessoas(evento){
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']");
 botoesGorjeta.forEach(botao=>{
-botao.addEventListener("click", receberPorcentagemBotao)
+botao.addEventListener("click", receberPorcentagem)
 })
 
-function receberPorcentagemBotao(evento){
+function receberPorcentagem(evento){
     botoesGorjeta.forEach(botao => {
         botao.classList.remove("botao-ativo")
 
@@ -40,6 +40,15 @@ function receberPorcentagemBotao(evento){
             botao.classList.add("botao-ativo")
         }
     })
-   porcentagem = parseFloat(evento.target.value) / 100
+
+if(evento.target.value !==""){
+    porcentagem = parseFloat(evento.target.value) / 100
+} else{
+porcentagem = 0
+}
+
 
 }
+
+const gorjetainput = document.querySelector("#outra")
+gorjetainput.addEventListener("input", receberPorcentagem)
